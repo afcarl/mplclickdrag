4acc9b2d669b697270373156c7937495aceacb3f
Matthew Johnson <mattjj@csail.mit.edu>
2015-09-14 07:28:40 -0400

    refactor into functions

--- viz.py
+++ viz.py
@@ -6,29 +6,34 @@ from matplotlib import cm
 
 
 class Interactive(object):
-    def __init__(self, initial_point):
+    def __init__(self):
         self.fig, (self.ax, self.imax) = fig, (ax, imax) = \
             plt.subplots(1, 2, figsize=(10, 5), facecolor='white')
         self.canvas = canvas = fig.canvas
 
-        ax.axis([-1,1,-1,1])
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax.autoscale(False)
-        self.circle = circle = Circle(
-            initial_point, radius=0.01, facecolor='r', animated=True)
-        ax.add_patch(circle)
-        self._dragging = False
-
-        imax.set_axis_off()
-        self.A = np.random.randn(900,2)
-        self.image = imax.imshow(np.random.randn(30,30), cmap=cm.YlGnBu)
-        imax.autoscale(False)
+        self._init_controlaxis(ax)
+        self._init_imageaxis(imax)
 
         canvas.mpl_connect('draw_event', self.draw_callback)
         canvas.mpl_connect('button_press_event', self.button_press_callback)
         canvas.mpl_connect('button_release_event', self.button_release_callback)
         canvas.mpl_connect('motion_notify_event', self.motion_notify_callback)
+
+    def _init_controlaxis(self, ax):
+        ax.axis([-1,1,-1,1])
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.autoscale(False)
+        self.circle = circle = Circle(
+            (0,0), radius=0.01, facecolor='r', animated=True)
+        ax.add_patch(circle)
+        self._dragging = False
+
+    def _init_imageaxis(self, imax):
+        imax.set_axis_off()
+        self.A = np.random.randn(900,2)
+        self.image = imax.imshow(np.random.randn(30,30), cmap=cm.YlGnBu)
+        imax.autoscale(False)
 
     def draw_callback(self, event):
         self.background = self.canvas.copy_from_bbox(self.ax.bbox)
@@ -44,8 +49,7 @@ class Interactive(object):
             return
 
         self._dragging = True
-        self._reposition_circle(event.xdata, event.ydata)
-        self._update_image()
+        self._update(event.xdata, event.ydata)
 
     def button_release_callback(self, event):
         if event.button == 1:
@@ -55,8 +59,11 @@ class Interactive(object):
         if event.inaxes is not self.ax or event.button != 1:
             return
         if self._dragging:
-            self._reposition_circle(event.xdata, event.ydata)
-            self._update_image()
+            self._update(event.xdata, event.ydata)
+
+    def _update(self, x, y):
+        self._reposition_circle(x, y)
+        self._update_image(x, y)
 
     def _reposition_circle(self, x, y):
         self.circle.center = x, y
@@ -64,13 +71,12 @@ class Interactive(object):
         self.ax.draw_artist(self.circle)
         self.canvas.blit(self.ax.bbox)
 
-    def _update_image(self):
-        x, y = self.circle.center
+    def _update_image(self, x, y):
         self.image.set_data(self.A.dot((x,y)).reshape((30,30)))
         self.imax.draw_artist(self.image)
         self.canvas.blit(self.imax.bbox)
 
 
 if __name__ == '__main__':
-    v = Interactive((0,0))
+    v = Interactive()
     plt.show()
