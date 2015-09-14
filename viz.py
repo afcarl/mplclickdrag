53192dc1c0d15abf3312c33fe36f8f33b235c2c5
Matthew Johnson <mattjj@csail.mit.edu>
2015-09-13 23:20:54 -0400

    dragging to tune a random image!

--- viz.py
+++ viz.py
@@ -1,15 +1,23 @@
 from __future__ import division
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
+from matplotlib import cm
 
+
+# TODO image doens't get updated
 
 class Interactive(object):
     def __init__(self, initial_point):
-        self.fig = fig = plt.figure(figsize=(5,5))
-        self.canvas = canvas = fig.canvas
-        self.ax = ax = plt.subplot(111)
+        self.fig, (ax, imax) = plt.subplots(2, 1, figsize=(5, 10), facecolor='white')
+        self.canvas = canvas = self.fig.canvas
 
+        # set up clicking subplot
+
+        self.ax = ax
+        ax.set_axis_off()
         ax.axis([-1,1,-1,1])
+        ax.autoscale(False)
 
         self.circle = Circle(
             initial_point, radius=0.01, facecolor='r', animated=True)
@@ -17,15 +25,27 @@ class Interactive(object):
 
         self._dragging = False
 
+        # set up image subplot
+
+        self.imax = imax
+        imax.set_axis_off()
+        self.A = np.random.randn(900,2)
+        self.image = imax.imshow(np.random.randn(30,30), cmap=cm.YlGnBu)
+        imax.autoscale(False)
+
         canvas.mpl_connect('draw_event', self.draw_callback)
         canvas.mpl_connect('button_press_event', self.button_press_callback)
         canvas.mpl_connect('button_release_event', self.button_release_callback)
         canvas.mpl_connect('motion_notify_event', self.motion_notify_callback)
 
     def draw_callback(self, event):
-        self.background = self.canvas.copy_from_bbox(self.fig.bbox)
+        self.background = self.canvas.copy_from_bbox(self.ax.bbox)
+
         self.ax.draw_artist(self.circle)
-        self.canvas.blit(self.fig.bbox)
+        self.imax.draw_artist(self.image)
+
+        self.canvas.blit(self.ax.bbox)
+        self.canvas.blit(self.imax.bbox)
 
     def button_press_callback(self, event):
         if event.inaxes is None or event.button != 1:
@@ -33,22 +53,30 @@ class Interactive(object):
 
         self._dragging = True
         self._reposition_circle(event.xdata, event.ydata)
+        self._update_image()
 
     def button_release_callback(self, event):
         if event.button == 1:
             self._dragging = False
 
     def motion_notify_callback(self, event):
-        if event.inaxes is None or event.button != 1:
+        if event.inaxes is not self.ax or event.button != 1:
             return
         if self._dragging:
             self._reposition_circle(event.xdata, event.ydata)
+            self._update_image()
 
     def _reposition_circle(self, x, y):
         self.circle.center = x, y
         self.canvas.restore_region(self.background)
         self.ax.draw_artist(self.circle)
-        self.canvas.blit(self.fig.bbox)
+        self.canvas.blit(self.ax.bbox)
+
+    def _update_image(self):
+        x, y = self.circle.center
+        self.image.set_data(self.A.dot((x,y)).reshape((30,30)))
+        self.imax.draw_artist(self.image)
+        self.canvas.blit(self.imax.bbox)
 
 
 if __name__ == '__main__':
